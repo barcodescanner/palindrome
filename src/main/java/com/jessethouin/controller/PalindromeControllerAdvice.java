002fa19b2cecf419e19c6a0dd7875d55a1fedc42
Jesse Thouin <jesse.thouin@medhost.com>
2016-06-11 16:04:52 -0500

    Made concessions for NASA's API.

--- src/main/java/com/jessethouin/controller/PalindromeControllerAdvice.java
+++ src/main/java/com/jessethouin/controller/PalindromeControllerAdvice.java
@@ -1,9 +1,11 @@
 package com.jessethouin.controller;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,6 +24,28 @@ public class PalindromeControllerAdvice {
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ResponseEntity<String> onUnhandledException(Exception e) {
 		String msg = "An uncaught exception occurred during the palindrome process. " + e.getMessage();
+		logger.error(msg, e);
+		return new ResponseEntity<>(msg, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public ResponseEntity<String> onUnhandledException(HttpMessageNotReadableException e) {
+		String msg = "The consistency of NASA's API leaves a lot to be desired. " +
+				"You're probably seeing this message because the Publications " +
+				"represented in the JSON response can have empty arrays inside " +
+				"of empty arrays, which should not be returned at all.\r\r" + e.getMessage();
+		logger.error(msg, e);
+		return new ResponseEntity<>(msg, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public ResponseEntity<String> onUnhandledException(JsonMappingException e) {
+		String msg = "The consistency of NASA's API leaves a lot to be desired. " +
+				"You're probably seeing this message because the Publications " +
+				"represented in the JSON response can have empty arrays inside " +
+				"of empty arrays, which should not be returned at all.\r\r" + e.getMessage();
 		logger.error(msg, e);
 		return new ResponseEntity<>(msg, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
--- src/main/java/com/jessethouin/model/Contact.java
+++ src/main/java/com/jessethouin/model/Contact.java
@@ -1,47 +1,57 @@
 
 package com.jessethouin.model;
 
-import javax.annotation.Generated;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
+import javax.annotation.Generated;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Generated("org.jsonschema2pojo")
 @JsonPropertyOrder({
-    "email",
+    "name",
     "office",
-    "facility"
+    "facility",
+    "phone",
+    "address",
+    "email"
 })
 public class Contact {
 
-    @JsonProperty("email")
-    private String email;
+    @JsonProperty("name")
+    private String name;
     @JsonProperty("office")
     private String office;
     @JsonProperty("facility")
     private String facility;
+    @JsonProperty("phone")
+    private String phone;
+    @JsonProperty("address")
+    private String address;
+    @JsonProperty("email")
+    private String email;
 
     /**
      * 
      * @return
-     *     The email
+     *     The name
      */
-    @JsonProperty("email")
-    public String getEmail() {
-        return email;
+    @JsonProperty("name")
+    public String getName() {
+        return name;
     }
 
     /**
      * 
-     * @param email
-     *     The email
+     * @param name
+     *     The name
      */
-    @JsonProperty("email")
-    public void setEmail(String email) {
-        this.email = email;
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
     }
 
     /**
@@ -84,9 +94,69 @@ public class Contact {
         this.facility = facility;
     }
 
+    /**
+     * 
+     * @return
+     *     The phone
+     */
+    @JsonProperty("phone")
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * 
+     * @param phone
+     *     The phone
+     */
+    @JsonProperty("phone")
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * 
+     * @return
+     *     The address
+     */
+    @JsonProperty("address")
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * 
+     * @param address
+     *     The address
+     */
+    @JsonProperty("address")
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * 
+     * @return
+     *     The email
+     */
+    @JsonProperty("email")
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 
+     * @param email
+     *     The email
+     */
+    @JsonProperty("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public int hashCode() {
-        return new HashCodeBuilder().append(email).append(office).append(facility).toHashCode();
+        return new HashCodeBuilder().append(name).append(office).append(facility).append(phone).append(address).append(email).toHashCode();
     }
 
     @Override
@@ -98,7 +168,7 @@ public class Contact {
             return false;
         }
         Contact rhs = ((Contact) other);
-        return new EqualsBuilder().append(email, rhs.email).append(office, rhs.office).append(facility, rhs.facility).isEquals();
+        return new EqualsBuilder().append(name, rhs.name).append(office, rhs.office).append(facility, rhs.facility).append(phone, rhs.phone).append(address, rhs.address).append(email, rhs.email).isEquals();
     }
 
 }
