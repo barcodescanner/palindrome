46f7666774708358075fcd985e8bf3d5318cd8bf
Jesse Thouin <jesse.thouin@medhost.com>
2016-06-10 22:12:33 -0500

    Initial commit

---
+++ src/main/java/com/jessethouin/PalindromeApplication.java
@@ -0,0 +1,12 @@
+package com.jessethouin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindromeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalindromeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jessethouin/PalindromeApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jessethouin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PalindromeApplication.class)
+@WebAppConfiguration
+public class PalindromeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
